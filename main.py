8c4dc68f7625253a139bdf5072f0d7438284d4e6
MFarisAdithya <m.farisadithya@gmail.com>
2025-05-31 01:20:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class TtkinterApp:
         bg_label = tk.Label(self.root, image=self.bg_photo)
         bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        start_button = tk.Button(self.root, text="Mulai", font=("Helvetica", 25, 'bold'), width=20, height=1, bg="#00aeae", fg='#000000', command=self.pages)
+        start_button = tk.Button(self.root, text="Mulai", font=("Helvetica", 25, 'bold'), width=20, height=1, bg="#00aeae", fg='#000000', command=self.pagesDua)
         start_button.place(relx=0.5, rely=0.8, anchor='center')  # Sesuaikan rely
 
         back_button = tk.Button(self.root,text="Kembali", font=("Helvetica", 13), bg='#C0C0C0', fg='black', command=self.main_menu)
